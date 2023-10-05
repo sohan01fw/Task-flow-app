e686cfb0af3938fdb9052b81d2f59ff384aff627
sohan01fw <sohan01fw@gmail.com>
2023-10-05 12:59:19 +0545

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="title">This is my Taskflow app with bun and hanko🚀🚀🚀</div>
+      <div className="title">
+        This is my Taskflow app with bun and hanko🚀🤖❤️😊
+      </div>
+      <div className="div">this is the description omg </div>
+      <div className="ok">and this is for nothing but test</div>
     </main>
-  )
+  );
 }
