fd0f21f927feaedaae67123726f185683abe1b4b
sohan01fw <sohan01fw@gmail.com>
2023-10-06 19:23:31 +0545

    figuring out to make dynamic coloring

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+"use client";
+import { colors } from "@/utils/Data_obj/Colors";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
+  const [color, setcolor] = useState("");
+  const handleClick = (data: string) => {
+    setcolor(data);
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
-      <div className="title dark:text-red-500">
+      <div className={`title text-${color}-500`}>
         This is my Taskflow app with bun and hankos🚀🤖❤️😊
       </div>
       <Link href="/login">
@@ -11,6 +19,22 @@ export default function Home() {
           Login to Taskflow
         </button>
       </Link>
+
+      <div className="colors">
+        {colors.map((data, index) => {
+          return (
+            <div
+              className="color p-2 border m-5 hover:cursor-pointer"
+              key={data}
+              onClick={() => {
+                handleClick(data);
+              }}
+            >
+              {data}
+            </div>
+          );
+        })}
+      </div>
     </main>
   );
 }
